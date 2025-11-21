5e2251db06fe198d45e0885ec2b1e93c6cdbf619
Alex Pestov <alexpestov89@yandex.ru>
2025-11-21 11:43:17 +0300

    Добавлен application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("java")
     id("com.github.ben-manes.versions") version "0.53.0"
+    id("application")
 }
 
 group = "hexlet.code"
@@ -8,6 +8,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+application {
+    mainClass = "hexlet.code.App"
 }
 
 tasks.test {
