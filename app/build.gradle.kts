1e5ad6cc7567aec1a3b9ebb390b3dcb57a93f18f
Alex Pestov <alexpestov89@yandex.ru>
2025-11-21 11:33:21 +0300

    Базовая конфигурация

---
+++ app/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("java")
+    id("com.github.ben-manes.versions") version "0.53.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
